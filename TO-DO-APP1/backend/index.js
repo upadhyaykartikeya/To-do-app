a716cb828134ac4041943ed18c3e83d645df35aa
upadhyaykartikeya <ukartik451@gmail.com>
2025-02-05 11:35:48 +0530

    new-year-app

---
+++ TO-DO-APP1/backend/index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.post('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
