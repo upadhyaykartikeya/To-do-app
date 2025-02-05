255f529a73fced0bd3023120473f0af628d2f27b
upadhyaykartikeya <ukartik451@gmail.com>
2025-02-05 14:42:20 +0530

    updated the code

--- TO-DO-APP1/backend/index.js
+++ TO-DO-APP1/backend/index.js
@@ -1,16 +1,37 @@
-const express = require('express')
+const express = require('express');
+const { createTodo, updateTodo } = require('./types');
 const app = express()
 const port = 3000
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.post('/todo', (req, res) => {
+  const createPayload = req.body;
+  const parsedPayload = createTodo.safeparse(createPayload);
+  if(!parsedPayload.success) {
+    res.status.json({
+      msg : "You sent the wrong input",
+    })
+    return;
+  }
+  
 })
 
-app.post('/', (req, res) => {
-  res.send('Hello World!')
+app.get("/todos", (req, res) => {
+  
 })
+
+app.post('/completed', (req, res) => {
+    const updatePayload = req.body;
+    const parsedPayload = updateTodo.safeParse(updatePayload);
+    if(!parsedPayload.success) {
+      res.status.json({
+        msg: "You sent the wrong input",
+      })
+      return;
+    }
+})
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
